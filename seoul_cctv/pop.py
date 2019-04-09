27276e1bac5739e744457d77614e1c0dfd063222
namgiho96 <namgiho96@gmail.com>
2019-04-09 17:56:33 +0900

    2019-04-09 17:56 텍스트마이닝

--- seoul_cctv/pop.py
+++ seoul_cctv/pop.py
@@ -2,27 +2,32 @@ import pandas as pd
 #  import folium
 
 ctx = '../data/'
-xls = ctx+'population_in_Seoul.xls'
-csv = ctx+'CCTV_in_Seoul.csv'
 
-pop_data = pd.read_excel(xls)
-cctv_data = pd.read_csv(csv)
+pop_data = pd.read_excel(ctx+'population_in_Seoul.xls'
+                         , encoding='UTF-8'
+                         , header=2
+                         , usecols='B,D,G,J,N')
+cctv_data = pd.read_csv(ctx+'CCTV_in_Seoul.csv')
 
-print(cctv_data.head())
-print(pop_data.head())
+cctv_data_schema = cctv_data.columns
+pop_data_schema = pop_data.columns
 
 """
-m = folium.Map(location=[37, -102], zoom_start=5)
-m.choropleth(
-    geo_data = xls,
-    name = 'choropleth',
-    data = data,
-    columns = ['State','population'],
-    key_on = 'feature.id',
-    fill_color = 'YlGn',
-    fill_opacity=0.7,
-    line_opacity = 0.2,
-    legend_name = 'population Rate (%)'
-)
-folium.LayerControl().add_to(m)
+cctv_data_schema
+['구별', '소계', '2013년도 이전', '2014년', '2015년', '2016년']
+
+pop_data_schema
+['구별', '인구수', '한국인', '외국인', '고령자']
 """
+
+cctv_data.rename(columns={cctv_data.columns[0]: '구별'}, inplace=True)
+# inplace=True 실제 변수의 내용을 바꿔라
+
+
+pop_data.rename(columns={pop_data.columns[0]:'구별'
+                         , pop_data.columns[1]:'인구수'
+                         , pop_data.columns[2]:'한국인'
+                         , pop_data.columns[3]:'외국인'
+                         , pop_data.columns[4]:'고령자'}
+                , inplace=True)
+print(pop_data.columns)
